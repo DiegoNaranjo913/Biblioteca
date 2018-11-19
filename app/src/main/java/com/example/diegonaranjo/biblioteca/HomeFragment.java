6b38419f7512527f4ffc5d89f902ba106a4364c2
DiegoNaranjo913 <30935581+DiegoNaranjo913@users.noreply.github.com>
2018-11-18 23:21:52 -0500

    modificaciones
    se le agrega un recycleview y se le agrega un splash y el icono de la aplicacion

--- app/src/main/java/com/example/diegonaranjo/biblioteca/HomeFragment.java
+++ app/src/main/java/com/example/diegonaranjo/biblioteca/HomeFragment.java
@@ -2,14 +2,20 @@ package com.example.diegonaranjo.biblioteca;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -20,7 +26,7 @@ import android.view.ViewGroup;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment implements View.OnClickListener {
+public class HomeFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,9 +35,9 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private CardView psicologia,sistemas,industrial,matematicas,negocios,mercadeo;
 
     private OnFragmentInteractionListener mListener;
+    private List<Carrera> carreras;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -70,22 +76,29 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
         View view =inflater.inflate(R.layout.fragment_home, container, false);
 
-        sistemas = (CardView) view.findViewById(R.id.sistemas);
-        psicologia = (CardView) view.findViewById(R.id.psicologia);
-        industrial = (CardView) view.findViewById(R.id.industrial);
-        matematicas = (CardView) view.findViewById(R.id.matematicas);
-        negocios = (CardView) view.findViewById(R.id.negocios);
-        mercadeo = (CardView) view.findViewById(R.id.mercadeo);
+        carreras = new ArrayList<>();
 
-        sistemas.setOnClickListener(this);
-        psicologia.setOnClickListener(this);
-        industrial.setOnClickListener(this);
-        matematicas.setOnClickListener(this);
-        negocios.setOnClickListener(this);
-        mercadeo.setOnClickListener(this);
+        carreras.add(new Carrera(R.drawable.psicologia,"Psicologia"));
+        carreras.add(new Carrera(R.drawable.sistemas,"Sistemas"));
+        carreras.add(new Carrera(R.drawable.industrial,"Industrial"));
+        carreras.add(new Carrera(R.drawable.matematicas,"Matematicas"));
+        carreras.add(new Carrera(R.drawable.negocios,"Negocios"));
+        carreras.add(new Carrera(R.drawable.mercadeo,"Mercadeo"));
+
+
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycle);
+        RecycleViewAdapter recycleViewAdapter = new RecycleViewAdapter(getContext(),carreras);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            recyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
+        }else {
+            recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        }
+        recyclerView.setAdapter(recycleViewAdapter);
         // Inflate the layout for this fragment
         return view;
     }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -111,30 +124,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         mListener = null;
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent i;
-        switch (v.getId()){
-            case R.id.sistemas:i = new Intent(getContext(),Sistemaslist.class);
-            startActivity(i);
-            break;
-            case R.id.psicologia:i = new Intent(getContext(),Psicologialist.class);
-            startActivity(i);
-            break;
-            case R.id.industrial:i = new Intent(getContext(),Industriallist.class);
-            startActivity(i);
-            break;
-            case R.id.matematicas:i = new Intent(getContext(),Matematicaslist.class);
-            startActivity(i);
-            break;
-            case R.id.negocios:i = new Intent(getContext(),Negocioslist.class);
-            startActivity(i);
-            break;
-            case R.id.mercadeo:i = new Intent(getContext(),Mercadeolist.class);
-            startActivity(i);
-            break;
-        }
-    }
+
 
     /**
      * This interface must be implemented by activities that contain this
